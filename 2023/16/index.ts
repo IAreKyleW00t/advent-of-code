e5f2ed2f28d3452d20766418190af1869e0b0cd9
Kyle Colantonio <k@yle.sh>
2023-12-16 17:29:47 -0500

    feat(2023): Saving Day 16 progress

--- 2023/16/index.ts
+++ 2023/16/index.ts
@@ -17,44 +17,102 @@ interface Beam {
   next?: Mirror;
 }
 
-// TODO: Energize up to edge
-function energize(grid: Grid, beam: Beam, mirror: Mirror): void {
-  // change cols
-  const ymin: number = Math.min(beam.y, mirror.y);
-  const ymax: number = Math.max(beam.y, mirror.y);
-  for (let i = ymin; i <= ymax; i++) grid[i][beam.x] = "#";
-
-  // change rows
-  const xmin: number = Math.min(beam.x, mirror.x);
-  const xmax: number = Math.max(beam.x, mirror.x);
-  for (let i = xmin; i <= xmax; i++) grid[beam.y][i] = "#";
+function startDirection(mirror: Mirror): Direction[] {
+  switch (mirror.type) {
+    case "/":
+      return ["L"];
+    case "\\":
+      return ["R"];
+    case "|":
+      return ["D"];
+    case "-":
+      return ["L", "R"];
+  }
+  return ["D"];
 }
 
-function next(beam: Beam, mirrors: Mirror[]): Mirror {
+function energize(grid: Grid, beam: Beam, mirror: Mirror | undefined): number {
+  let yedge: number;
+  let xedge: number;
+
+  // handle edges
+  if (!mirror) {
+    switch (beam.direction) {
+      case "U":
+        yedge = 0;
+        xedge = beam.x;
+        break;
+      case "D":
+        yedge = grid.length - 1;
+        xedge = beam.x;
+        break;
+      case "L":
+        yedge = beam.y;
+        xedge = 0;
+        break;
+      case "R":
+        yedge = beam.y;
+        xedge = grid[0].length - 1;
+        break;
+    }
+  } else {
+    yedge = mirror.y;
+    xedge = mirror.x;
+  }
+
+  // number of tiles that were engergized
+  let count: number = 0;
+
+  // change cols
+  let ymin: number = Math.min(beam.y, yedge);
+  let ymax: number = Math.max(beam.y, yedge);
+  for (let i = ymin; i <= ymax; i++) {
+    if (grid[i][beam.x] !== "#") count++;
+    grid[i][beam.x] = "#";
+  }
+
+  // change rows
+  let xmin: number = Math.min(beam.x, xedge);
+  let xmax: number = Math.max(beam.x, xedge);
+  for (let i = xmin; i <= xmax; i++) {
+    if (grid[beam.y][i] !== "#") count++;
+    grid[beam.y][i] = "#";
+  }
+
+  return count;
+}
+
+function next(beam: Beam, mirrors: Mirror[]): Mirror | undefined {
   switch (beam.direction) {
     case "U":
-      return mirrors.filter((m) => m.x === beam.x && m.y < beam.y)[0];
+      return mirrors.filter((m) => m.x === beam.x && m.y < beam.y).pop();
     case "D":
-      return mirrors.filter((m) => m.x === beam.x && m.y > beam.y)[0];
+      return mirrors.filter((m) => m.x === beam.x && m.y > beam.y).shift();
     case "L":
-      return mirrors.filter((m) => m.y === beam.y && m.x < beam.x)[0];
+      return mirrors.filter((m) => m.y === beam.y && m.x < beam.x).pop();
     case "R":
-      return mirrors.filter((m) => m.y === beam.y && m.x > beam.x)[0];
+      return mirrors.filter((m) => m.y === beam.y && m.x > beam.x).shift();
   }
 }
 
-// TODO: handle \ and /
-function beam(grid: Grid, mirrors: Mirror[], beams: Beam[]): void {
+function beam(grid: Grid, mirrors: Mirror[], beams: Beam[]): number {
   // find the next location of each beam and energize blocks between
+  const history: string[] = [];
+  let count: number = 0;
   while (beams.length > 0) {
     let b: Beam = beams[0];
-    console.log(beams);
-    console.log(`next => ${b.next?.type}`);
-    pprint(grid);
-
     b.next = next(b, mirrors);
-    if (b.next) energize(grid, b, b.next);
 
+    //pprint(grid);
+
+    // check if we've already been here
+    const key: string = `${b.next?.type}${b.x}${b.y}${b.direction}`;
+    if (history.includes(key)) {
+      beams.shift();
+      continue;
+    }
+
+    count += energize(grid, b, b.next);
     if (b.next?.type === "|") {
       if (["L", "R"].includes(b.direction)) {
         // split vertically
@@ -71,8 +129,32 @@ function beam(grid: Grid, mirrors: Mirror[], beams: Beam[]): void {
       } else {
         beams[0] = { x: b.next.x, y: b.next.y, direction: b.direction };
       }
+    } else if (b.next?.type === "/") {
+      if (b.direction === "U") {
+        beams[0] = { x: b.next.x, y: b.next.y, direction: "R" };
+      } else if (b.direction === "L") {
+        beams[0] = { x: b.next.x, y: b.next.y, direction: "D" };
+      } else if (b.direction === "D") {
+        beams[0] = { x: b.next.x, y: b.next.y, direction: "L" };
+      } else if (b.direction === "R") {
+        beams[0] = { x: b.next.x, y: b.next.y, direction: "U" };
+      }
+    } else if (b.next?.type === "\\") {
+      if (b.direction === "U") {
+        beams[0] = { x: b.next.x, y: b.next.y, direction: "L" };
+      } else if (b.direction === "R") {
+        beams[0] = { x: b.next.x, y: b.next.y, direction: "D" };
+      } else if (b.direction === "D") {
+        beams[0] = { x: b.next.x, y: b.next.y, direction: "R" };
+      } else if (b.direction === "L") {
+        beams[0] = { x: b.next.x, y: b.next.y, direction: "U" };
+      }
     } else beams.shift();
+
+    history.push(key);
   }
+
+  return count;
 }
 
 function pprint(grid: Grid) {
@@ -95,19 +177,43 @@ function part1(input: string[]): number {
   });
 
   // Create the initial beam in the top-left, moving right
-  const initial: Beam[] = [{ x: 0, y: 0, direction: "R" }];
-  beam(grid, mirrors, initial);
-
-  let total: number = 0;
-  return total;
+  const initial: Beam[] = [{ x: 0, y: 0, direction: "D" }];
+  return beam(grid, mirrors, initial);
 }
 
 function part2(input: string[]): number {
-  let total: number = 0;
-  input.forEach((line) => {
+  const grid: Grid = [];
+  const mirrors: Mirror[] = [];
+  input.forEach((line, y) => {
     if (!line) return; // skip empty lines
+
+    grid[y] = [];
+    line.split("").forEach((c, x) => {
+      grid[y][x] = c as Tile;
+      if (["\\", "/", "|", "-"].includes(c))
+        mirrors.push({ x: x, y: y, type: c as Tile });
+    });
   });
-  return total;
+
+  // Create the initial beam in the top-left, moving right
+  let beams: number[] = [];
+  for (let i = 0; i < grid[0].length; i++) {
+    const g: Grid = Object.assign([], grid);
+    const mirror: Mirror | undefined = mirrors
+      .filter((m) => m.x === i && m.y === 0)
+      .pop();
+
+    let initial: Beam[] = [];
+    if (mirror) {
+      initial = startDirection(mirror).map((d) => ({
+        x: i,
+        y: 0,
+        direction: d,
+      }));
+    } else initial = [{ x: i, y: 0, direction: "D" }];
+    beams.push(beam(g, mirrors, initial));
+  }
+  return Math.max(...beams);
 }
 
 const stdin: string[] = fs.readFileSync(0).toString().split(/\r?\n/);
