1fc5ea981c7186324e8b8e507935a0c236224811
Kyle Colantonio <k@yle.sh>
2023-12-19 21:56:35 -0500

    feat(2023): Cleaning up some Day 19

--- 2023/19/index.ts
+++ 2023/19/index.ts
@@ -1,17 +1,17 @@
 import * as fs from "fs";
 
-type Operation = ">" | "<";
+type Operation = "<" | ">";
 
-interface Rating {
+interface Category {
   name: string;
   value: number;
 }
 
 class Part {
-  ratings: Rating[];
+  categories: Category[];
 
   constructor(line: string) {
-    this.ratings = line
+    this.categories = line
       .slice(1, -1) // remove { }
       .split(",")
       .map((r) => {
@@ -21,125 +21,109 @@ class Part {
   }
 
   sum(): number {
-    return this.ratings.reduce((sum, v) => (sum += v.value), 0);
+    return this.categories.reduce((sum, v) => (sum += v.value), 0);
   }
 }
 
-class State {
-  rating?: Rating;
+class Rule {
+  category?: Category;
   op?: Operation;
-  next: string;
+  state: string;
 
   constructor(str: string) {
-    // Parse the stage operation
-    // if none is provided, then consider this an end state
     const op: RegExpMatchArray | null = str.match(/<|>/);
     if (op) {
       this.op = op[0] as Operation;
       let split: string[] = str.split(":");
-      this.next = split[1];
+      this.state = split[1];
 
       split = split[0].split(this.op);
-      this.rating = { name: split[0], value: parseInt(split[1]) };
-    } else this.next = str;
+      this.category = { name: split[0], value: parseInt(split[1]) };
+    } else this.state = str;
   }
 
   isEnd(): boolean {
-    return !this.rating && !this.op;
+    return !this.category && !this.op;
   }
 
   process(part: Part): boolean {
-    // if this is an end state then it will accept
-    // anything that falls into it
-    if (this.isEnd()) return true;
+    for (let i = 0; i < part.categories.length; i++) {
+      const rule: Category = part.categories[i];
+      if (this.category!.name !== rule.name) continue;
 
-    // otherwise actually check the state
-    for (let i = 0; i < part.ratings.length; i++) {
-      const r: Rating = part.ratings[i];
-      if (this.rating!.name === r.name) {
-        if (this.op === ">") return r.value > this.rating!.value;
-        else if (this.op === "<") return r.value < this.rating!.value;
-      }
+      if (this.op === ">") return rule.value > this.category!.value;
+      else if (this.op === "<") return rule.value < this.category!.value;
     }
     return false;
   }
 }
 
-class Workflow {
+class State {
   name: string;
-  states: State[];
+  rules: Rule[];
 
   constructor(line: string) {
     const match: RegExpMatchArray = line.match(/(\w+)\{(.+)\}/)!;
     this.name = match[1];
-    this.states = [];
-    match[2].split(",").forEach((stage) => {
-      this.states.push(new State(stage));
+    this.rules = [];
+    match[2].split(",").forEach((rule) => {
+      this.rules.push(new Rule(rule));
     });
   }
 
-  process(part: Part): string | undefined {
-    // run the part through each state until we
+  process(part: Part): string {
+    // run the part through each rule until we
     // hit one that is valid, otherwise this is invalid
-    for (let i = 0; i < this.states.length; i++) {
-      const stage: State = this.states[i];
-      const valid: boolean = stage.process(part);
-      if (valid) return stage.next;
+    for (let i = 0; i < this.rules.length; i++) {
+      const rule: Rule = this.rules[i];
+      // if this is an end state then it will accept anything
+      if (rule.isEnd()) return rule.state;
+
+      // otherwise actually check the rule
+      if (rule.process(part)) return rule.state;
     }
-    return undefined;
+    return "R";
   }
 }
 
-function processParts(
-  workflows: Map<string, Workflow>,
-  parts: Set<Part>
+function processPart(
+  states: Map<string, State>,
+  part: Part,
+  start: string = "in"
 ): number {
-  const accepted: number[] = [];
-  parts.forEach((part) => {
-    let workflow: Workflow = workflows.get("in")!; // start at "in" state
-    let next: string | undefined;
-    do {
-      next = workflow.process(part); // run the workflow
-      if (next) {
-        // if we're at an Accepted or Rejected state
-        // then we can stop, otherwise keep moving through
-        // the workflows
-        if (next === "A") {
-          accepted.push(part.sum());
-          return;
-        } else if (workflows.has(next)) {
-          workflow = workflows.get(next)!;
-        } else return;
-      }
-    } while (next);
-  });
-  return accepted.reduce((sum, a) => (sum += a), 0);
+  let current: string = start;
+  while (current) {
+    const state = states.get(current);
+    if (!state) return 0; // invalid
+
+    // process part and then check if we're
+    // at an Accepted or Rejected state
+    current = state.process(part);
+    if (current === "A") return part.sum();
+    else if (current === "R") return 0;
+  }
+  return 0; // invalid
 }
 
 function part1(input: string[]): number {
-  // could use a normal object with dynamic keys, but Maps are
-  // a bit faster if you're only needing key:value pairs
-  const workflows: Map<string, Workflow> = new Map<string, Workflow>();
+  const states: Map<string, State> = new Map<string, State>();
   input[0].split(/\r?\n/).forEach((line) => {
-    const w: Workflow = new Workflow(line);
-    workflows.set(w.name, w);
+    const s: State = new State(line);
+    states.set(s.name, s);
   });
 
-  // could use a normal array, but Sets are a bit faster
-  // if you don't need exact indexes
-  const parts: Set<Part> = new Set<Part>();
+  let total: number = 0;
   input[1].split(/\r?\n/).forEach((line) => {
-    parts.add(new Part(line));
+    total += processPart(states, new Part(line));
   });
-
-  return processParts(workflows, parts);
+  return total;
 }
 
 function part2(input: string[]): number {
-  const workflows: Map<string, Workflow> = new Map<string, Workflow>();
+  const states: Map<string, State> = new Map<string, State>();
   input[0].split(/\r?\n/).forEach((line) => {
-    const w: Workflow = new Workflow(line);
-    workflows.set(w.name, w);
+    const s: State = new State(line);
+    states.set(s.name, s);
   });
 
   return 0;
