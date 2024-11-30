f5ed16f096d3e894b24af75702770ec9061818f8
Kyle Colantonio <k@yle.sh>
2024-11-30 11:39:09 -0500

    feat: Updating README and base

---
+++ 2024/01/README.md
@@ -0,0 +1,8 @@
+# Day 1 ${\color{gray}★★}$
+
+https://adventofcode.com/2024/day/1
+
+```
+asdf install
+go run main.go < input.txt
+```
--- 2024/01/main.go
+++ 2024/01/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	log.SetOutput(os.Stdout) // Log to stdout instead of stderr
+
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		log.Println(input.Text())
+	}
+
+	p1 := time.Now()
+	part1()
+	p2 := time.Now()
+	part2()
+
+	log.Println("================================================================")
+	log.Printf("Part 1: %s", time.Since(p1))
+	log.Printf("Part 2: %s", time.Since(p2))
+}
+
+func part1() int {
+	return 0
+}
+
+func part2() int {
+	return 0
 }
